20af14e98f7e8ffa2b14c24d19841978d2d79393
Rob Fowler <rtf04@outlook.com>
2023-04-09 15:39:37 -0400

    initial commit

---
+++ config.py
@@ -0,0 +1,29 @@
+"""
+LedgerAssistant - a GUI front end for entering Ledger-compatible transactions.
+Copyright (C) 2023  Robert T. Fowler IV
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+import configparser
+from pathlib import Path
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+journal_file = Path(config['transactions']['journal'])
+if config['accounts']['source'] == 'file':
+    journal_file = Path(config['accounts']['file'])
+    with open(journal_file,"r",encoding='utf-8') as f:
+        accounts = [account for account in list(f.read().split("\n"))]
+
